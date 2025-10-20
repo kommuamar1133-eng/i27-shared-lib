b947c1a1878b406fe6f7adbab19169f7e89e330d
charyacomail <charyacomail@gmail.com>
2025-10-20 20:21:41 +0530

    Changed pipelineParams

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -3,7 +3,7 @@
 import com.i27academy.builds.Calculator
 import com.i27academy.builds.Docker
 
-def call(Map pipelineparams){
+def call(Map pipelineParams){
     // An instance of the class called calculator is created
     Calculator calculator = new Calculator(this)
     Docker docker = new Docker(this)
